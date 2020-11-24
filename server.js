fd9fe78b9d092422ac80a7f3403c89cee60f1b82
Nizan Mandelblit <nizanm91@gmail.com>
2020-11-24 14:46:13 +0200

    addes index and folders

--- server.js
+++ server.js
@@ -1,35 +1,45 @@
 const express = require("express")
-const bodyParser=require("body-parser")
+const bodyParser = require("body-parser")
 const app = express()
-app.set("view engine","ejs");
-app.use( express.static( "public" ) );
-
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 const port = 3001
 var pageName;
 
-app.get('/', (req, res) => {
-    pageName="home page";
 
-    res.render("index", {pageName:pageName});
+// GET functions
+app.get('/', (req, res) => {
+    pageName = "home page";
+
+    res.render("index", {pageName: pageName});
 })
 
 app.get('/search', (req, res) => {
-    pageName="search page";
+    pageName = "search page";
 
-    res.render("index", {pageName:pageName});
+    res.render("index", {pageName: pageName});
 })
 
 app.get('/update', (req, res) => {
-    pageName="update page";
+    pageName = "update page";
 
-    res.render("index", {pageName:pageName});
+    res.render("index", {pageName: pageName});
 })
 
 app.get('/delete', (req, res) => {
-    pageName="delete page";
+    pageName = "delete page";
 
-    res.render("index", {pageName:pageName});
+    res.render("index", {pageName: pageName});
 })
+
+
+//POST functions
+app.post("/delete", function (req, res) {
+    var id = req.body.id;
+    console.log(id);
+})
+
 
 app.listen(process.env.PORT | port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
