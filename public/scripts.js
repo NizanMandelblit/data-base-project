12eca865fdc595197e59ed407fee735eb630d90a
Nizan Mandelblit <nizanm91@gmail.com>
2020-12-15 15:05:28 +0200

    added filter search box to /search

--- public/scripts.js
+++ public/scripts.js
@@ -7,8 +7,22 @@ $(document).ready(function(){
 
 
 
+$(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myTable tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
 
-function validate() {
-    return true;
-}
 
+
+$(document).ready(function(){
+    $("#myInput2").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myTable2 tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
