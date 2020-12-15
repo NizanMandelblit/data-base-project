410ff973a1e9507e0d509230d4604b3ad8b1d08e
Nizan Mandelblit <nizanm91@gmail.com>
2020-12-15 14:13:56 +0200

    fixed loading animation

--- public/scripts.js
+++ public/scripts.js
@@ -1,26 +1,14 @@
-function showHide() {
+$(document).ready(function(){
+    $("#my-form").on("submit", function(){
+        $("#loadingbtn").show()
+        $("#sumbitbtn").hide()
+    });//submit
+});//dom ready
 
-//create an object reference to the div containing images
-    var oimageDiv = document.getElementById('loadingbtn')
-//set display to inline if currently none, otherwise to none
-    if (document.getElementById("requirelist").selectedIndex >= 0) {
-        oimageDiv.style.display = 'inline'
-        setTimeout(function () {
-            oimageDiv.innerText = "please fill in the required fields!"
-        }, 1000)
-        //  document.getElementById("sumbitbtn").style.display = 'none'
 
-    }
+
+
+function validate() {
+    return true;
 }
 
-
-function showHiderglr() {
-//create an object reference to the div containing images
-    var oimageDiv = document.getElementById('loadingbtn')
-//set display to inline if currently none, otherwise to none
-    oimageDiv.style.display = 'inline'
-    setTimeout(function () {
-        oimageDiv.innerText = "please fill in the required fields!"
-    }, 1000)
-    //document.getElementById("sumbitbtn").style.display = 'none'
-}
